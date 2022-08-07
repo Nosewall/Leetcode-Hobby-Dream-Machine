17e7fbd8b66b7b25a84e392adeb2030d63ef198e
Nolan Nordwall <NolanNordwall@gmail.com>
2022-08-06 23:56:03 -0700

    Added 69. sqrt. 8/6/2022

---
+++ 69. Sqrt/main.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def mySqrt(self, x):
+        low = 0
+        high = x
+        while low <= high:
+            #The double slash operator gives you the result of a division operation as a whole number
+            middle = (high + low) // 2
+            sqr = middle * middle
+
+            if sqr == x:
+                return middle
+            elif sqr < x:
+                low = middle + 1
+            else:
+                high = middle - 1
+
+        return high
+
+
+sol = Solution()
+print(sol.mySqrt(8))
