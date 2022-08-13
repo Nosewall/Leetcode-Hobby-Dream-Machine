e1feaff16022f3d6d56c9771e56704ba392fb1a1
Nolan Nordwall <NolanNordwall@gmail.com>
2022-08-13 14:21:32 -0700

    Added 101. Symetrical Tree. 8/13/2022

---
+++ 101. Symmetric Tree/main.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.check(root.left, root.right)
+
+    def check(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+
+        if left.val == right.val:
+            return self.check(left.left, right.right) and self.check(left.right, right.left)
+        else:
+            return False
+
