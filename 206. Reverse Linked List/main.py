2f851dec7c0fccb601e73f975f2f0a3032c5679f
Nolan Nordwall <NolanNordwall@gmail.com>
2022-09-06 17:15:49 -0700

    Added 206. Reverse Linked List

---
+++ 206. Reverse Linked List/main.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+        return prev
