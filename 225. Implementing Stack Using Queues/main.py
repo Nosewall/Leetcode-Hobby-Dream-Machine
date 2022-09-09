9cabcadf218670d9d3c7061764eb1779c5bf60cb
Nolan Nordwall <NolanNordwall@gmail.com>
2022-09-09 10:12:29 -0700

    Added 225. Implementing stack using queues. 9/9/2022

---
+++ 225. Implementing Stack Using Queues/main.py
@@ -0,0 +1,36 @@
+class MyStack(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.insert(0, x)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop(0)
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return len(self.stack) == 0
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
