e751cb6a2d5d2f003ed8603f79c80a40fe5cd41d
Nolan Nordwall <NolanNordwall@gmail.com>
2022-09-13 11:03:44 -0700

    Added 232. Implement Queue Using Stacks. 9/13/2022

---
+++ 232. Implement Queue using Stacks/main.py
@@ -0,0 +1,36 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.insert(len(self.stack), x)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop(0)
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.stack[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return len(self.stack) == 0
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
