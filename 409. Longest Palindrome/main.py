b2e997d12688011caa51dd0f82af759449fadddc
Nolan Nordwall <NolanNordwall@gmail.com>
2022-10-20 14:21:54 -0700

    Added 409. Longest Palindrome. 10/20/2022

---
+++ 409. Longest Palindrome/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        map = set()
+
+        doubles = 0
+        for i in s:
+            if i in map:
+                doubles += 1
+                map.remove(i)
+            else:
+                map.add(i)
+        if len(map) == 0:
+            return doubles * 2
+        else:
+            return (doubles * 2) + 1
+
+
+sol = Solution()
+print(sol.longestPalindrome("aab098yth"))
