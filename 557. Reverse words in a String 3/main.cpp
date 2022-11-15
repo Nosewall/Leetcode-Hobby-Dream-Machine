b83c0e86eb1798296a8b26b997f489f1675457df
Nolan Nordwall <NolanNordwall@gmail.com>
2022-11-15 00:18:15 -0800

    Added 557. Reverse Words In A String III. 11/15/2022

---
+++ 557. Reverse words in a String 3/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i=0;
+        for(int j=0;j<s.length();j++)
+        {
+            if(s[j]==' ')
+            {
+                reverse(s.begin()+i,s.begin()+j);
+                i=j+1;
+            }
+        }
+        reverse(s.begin()+i,s.end());
+        return s;
+    }
+};
