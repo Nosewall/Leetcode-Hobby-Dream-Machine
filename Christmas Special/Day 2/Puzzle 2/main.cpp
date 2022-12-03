ff2ed45b6bbffb1e6c036a9fa0a4d9a13adfa0c2
Nolan Nordwall <NolanNordwall@gmail.com>
2022-12-02 22:00:37 -0800

    Finished Day 2 Puzzle 2

---
+++ Christmas Special/Day 2/Puzzle 2/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <cctype>
+#include <locale>
+#include <vector>
+
+int getScoreBasedOnPair(char opponent, char you){
+    if (opponent == 'A'){
+        if (you == 'X'){
+            return 3;
+        } else if (you == 'Y'){
+            return 4;
+        } else if (you == 'Z'){
+            return 8;
+        }
+    }
+    else if (opponent == 'B'){
+        if (you == 'X'){
+            return 1;
+        } else if (you == 'Y'){
+            return 5;
+        } else if (you == 'Z'){
+            return 9;
+        }
+    }
+    else if (opponent == 'C'){
+        if (you == 'X'){
+            return 2;
+        } else if (you == 'Y'){
+            return 6;
+        } else if (you == 'Z'){
+            return 7;
+        }
+    }
+}
+
+int main(){
+    int totalScore = 0;
+    std::vector<char> opponentMoves;
+    std::vector<char> yourMoves;
+    std::ifstream strategyGuide("RPS.txt");
+    std::string line;
+    while(std::getline(strategyGuide, line)){
+        opponentMoves.push_back(line[0]);
+        yourMoves.push_back(line[2]);
+        totalScore += getScoreBasedOnPair(line[0], line[2]);
+    }
+    printf("Your final score following the NEW strategy is : %d", totalScore);
+    return 0;
+}
