5d9d565ec4f35f10a9823f18847d7eb7d9233a1a
Nolan Nordwall <NolanNordwall@gmail.com>
2022-12-03 23:08:47 -0800

    Finished day 4 puzzle 2

--- Christmas Special/Day 4/Puzzle 2/main.cpp
+++ Christmas Special/Day 4/Puzzle 2/main.cpp
@@ -1,3 +1,64 @@
-//
-// Created by Nolan on 12/3/2022.
-//
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+using std::string;
+using std::tuple;
+using std::ifstream;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::get;
+using std::stoi;
+
+tuple<string, string> getBothPairs(string inputLine, char delimiter){
+    //std::tuple<std::string> pairs = ("sds", "ndshkaf");
+    for(int i = 0; i < inputLine.length(); i++){
+        if(inputLine[i] == delimiter){
+            tuple<string, string> pairs = std::make_tuple(
+                    inputLine.substr(0,i),
+                    inputLine.substr(i + 1)
+            );
+            return pairs;
+        }
+    }
+}
+
+bool doesPairContainTotalOverlap(int elf1Start, int elf1End, int elf2Start, int elf2End){
+    if (elf1Start >= elf2Start && elf1End <= elf2End || elf2Start >= elf1Start && elf2End <= elf1End){
+        return true;
+    }
+    return false;
+}
+
+bool doesPairContainAnyOverlap(int e1s, int e1e, int e2s, int e2e){
+    if(doesPairContainTotalOverlap(e1s, e1e, e2s, e2e) ||
+        e1s <= e2s && e1e >= e2s || e2s <= e1s && e2e >= e1s){
+        return true;
+    }
+    return false;
+}
+
+
+
+int main(){
+    int allTotalOverlap = 0;
+    ifstream allPairs("clean.txt");
+    string line;
+    while(std::getline(allPairs, line)){
+        tuple<string, string> pairs = getBothPairs(line, ',');
+        tuple<string, string> elf1 = getBothPairs(get<0>(pairs), '-');
+        tuple<string, string> elf2 = getBothPairs(get<1>(pairs), '-');
+        int elf1Start = stoi(get<0>(elf1));
+        int elf1End = stoi(get<1>(elf1));
+        int elf2Start = stoi(get<0>(elf2));
+        int elf2End = stoi(get<1>(elf2));
+        if (doesPairContainAnyOverlap(elf1Start, elf1End, elf2Start, elf2End)){
+            allTotalOverlap++;
+        }
+    }
+    cout << "Total pairs that have any overlap : " << allTotalOverlap << endl;
+
+    return 0;
+}
